db813c8eae5cacf92d9e7ae55d70e06cde81a801
Tyler Willaims <twilliams18@bellarmine.edu>
2024-04-10 13:44:26 -0400

    Initial commit

---
+++ gitDemo.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author Tyler Williams
+ * 
+ */
+public class gitDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println ("Hello Dr. Johnson! This is Tyler WIlliams.");
+		
+		
+		
+	}
+
+}
